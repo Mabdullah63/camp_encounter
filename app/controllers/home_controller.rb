4b191acf7033d079dd57134375b44e89ff312883
Mabdullah63 <muhammad.abdullah@square63.org>
2021-11-03 12:49:32 +0500

    fixes: signup and login working properly

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,4 @@
 class HomeController < ApplicationController
   def index
-    redirect_to new_user_registration_url
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   #get 'home/index'
   devise_for :users
-  root 'home#index'
+  devise_scope :user do
+      unauthenticated do
+        root 'users/registrations#new', as: :unauthenticated_root
+      end
+      authenticated do
+        root 'home#index'
+      end
+    end
+    #root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
