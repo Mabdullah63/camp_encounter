77679d75f595e7aa8d2a3414b242b33b59a81753
Mabdullah63 <muhammad.abdullah@square63.org>
2021-11-11 15:37:25 +0500

    feat: adminuser_profile and edit_admin_user_profile is added

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -1,4 +1,7 @@
 class Admins::UsersController < AdminsController
+before_action :authenticate_user!
+before_action :set_user, only: [:show]
+before_action :correct_user, only: [:edit, :update, :destroy]
   def index
     if params[:query].present?
       @pagy, @users = pagy(User.search(params[:query]).order(:id))
@@ -8,6 +11,7 @@ class Admins::UsersController < AdminsController
       @pagy, @users = pagy(User.all.order(:id))
     end
   end
+
   def destroy
     @user = User.find(params[:id])
     if @user.destroy
@@ -17,4 +21,38 @@ class Admins::UsersController < AdminsController
       end
     end
   end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to admins_users_path, notice: "User was successfully updated." }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def correct_user
+    @user=User.find_by(id: params[:id])
+    redirect_to admins_users_path, notice: "Not authorized to edit this user" if @user.nil? || @user.admin?
+  end
+
+
+  private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :phone, :country_code, :user_id, :country)
+    end
 end
