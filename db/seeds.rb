a56abd06a5e75e7ccac0c0c549402cfc2cfe7c5b
Mabdullah63 <muhammad.abdullah@square63.org>
2021-11-16 15:27:00 +0500

    PR1 issues resolved

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(email: "admin@projectname.com", password: "Password123!@#", role: "admin" , first_name: "Khurram", last_name: "Sajjad", phone: "336-514-4430", country: "Pakistan",country_code: "92")
+User.first_or_create(email: "admin@projectname.com", password: "Password123!@#", role: "admin" , first_name: "Khurram", last_name: "Sajjad", phone: "336-514-4430", country: "Pakistan",country_code: "92")
