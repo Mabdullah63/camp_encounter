a21a3b8a7b632531b79ea50f8555216a916c73fb
Mabdullah63 <92972726+Mabdullah63@users.noreply.github.com>
2021-11-25 08:06:40 +0500

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
-
     if @user.destroy
       respond_to do |format|
         format.html { redirect_to admins_path, notice: "User successfully destroyed." }
