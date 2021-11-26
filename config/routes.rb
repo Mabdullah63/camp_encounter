16675b66f44f645b0a19abea9f1946c9bc676689
Mabdullah63 <muhammad.abdullah@square63.org>
2021-11-26 17:04:41 +0500

    fixes: PR1 issue resolved

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   resources :admins
   devise_scope :user do
-      unauthenticated do
-        root 'users/registrations#new', as: :unauthenticated_root
-      end
-      authenticated do
-        root 'home#index'
-      end
+    unauthenticated do
+      root 'users/registrations#new', as: :unauthenticated_root
     end
+    authenticated do
+      root 'home#index'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
